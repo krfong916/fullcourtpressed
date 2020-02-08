272c76ced592f0695f4cbea087a2ee093eed511f
Kyle Fong <krfong@ucsc.edu>
2020-02-07 22:34:45 -0800

    add 7

---
+++ leet/java/7-reverse-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+  public int reverse(int n) {
+    long reverse = 0;
+    
+    while (n != 0) {
+      reverse = (n % 10) + (reverse * 10);
+      n /= 10;
+    }
+    if (reverse < Integer.MIN_VALUE || reverse > Integer.MAX_VALUE) {
+      return 0;
+    } else {
+      return (int)reverse;
+    }
+  }
+}
