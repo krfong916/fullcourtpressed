5592be50d390897c10f4f51fb578a0d8090c1d84
Kyle Fong <krfong@ucsc.edu>
2020-02-11 21:53:00 -0800

    formatting

--- leet/java/64-minimum-path-sum-a.java
+++ leet/java/64-minimum-path-sum-a.java
@@ -6,17 +6,17 @@ class Solution {
     dp[0][0] = grid[0][0];
 
     for (int i = 1; i < grid.length; i++) {
-        dp[i][0] = dp[i-1][0] + grid[i][0];
+      dp[i][0] = dp[i-1][0] + grid[i][0];
     }
 
     for (int j = 1; j < grid[0].length; j++) {
-        dp[0][j] = dp[0][j-1] + grid[0][j];
+      dp[0][j] = dp[0][j-1] + grid[0][j];
     }
 
     for (int i = 0; i < grid.length; i++) {
-        for (int j = 0; j < grid[0].length; j++) {
-            dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
-        }
+      for (int j = 0; j < grid[0].length; j++) {
+        dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+      }
     }
     
     return dp[m-1][n-1];
