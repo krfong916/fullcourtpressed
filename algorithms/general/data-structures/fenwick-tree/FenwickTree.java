99af0ad705633724c72004939a9509745b0c609a
Kyle Fong <krfong@ucsc.edu>
2020-05-05 01:55:34 -0700

    Add psuedocode for constructing a Fenwick tree

--- algorithms/general/data-structures/fenwick-tree/FenwickTree.java
+++ algorithms/general/data-structures/fenwick-tree/FenwickTree.java
@@ -1,6 +1,16 @@
 class FenwickTree {
 	public FenwickTree() {}
-	public FenwickTree(long[] values) {}
+	public FenwickTree(long[] values) {
+    // get length of the values array, n
+    // our fenwick tree will be 1-based, instead of being 0-based
+    // make a deep copy of the values array so that we don't modify the values array while we construct the fenwick tree, call this tree
+    // for the length of the tree, starting at 1 to tree.length
+      // add the current value of i to the parent of i
+      // parent = i + lsb(i)
+      // if parent <= tree.length
+        // tree[parent] += tree[i]
+    // return tree
+  }
 
 	public int rangeQuery(int begin, int end) {
 		// if begin >= 1 and end >= 1, if begin >= getSize() || end >= getSize(), throw an exception
@@ -17,13 +27,15 @@ class FenwickTree {
       // i -= lsb(i)
     // return prefixSum
   }
+
 	public void rangeUpdate(int i, int x) {
 		// if i >= getSize() && i >= 1, throw an exception
-		// while i < getSize()
+		// while i <= getSize()
 			// add x to r[i]
 			// get least significant bit of i -> lsb(i)
 			// i += lsb(i)
 	}
+
 	public String serialize() {}
 	public void deserialize() {}
 	private int getSize() {}
