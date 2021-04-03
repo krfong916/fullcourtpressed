8210f62c0dedcec17b372e3e53f2f7e2556643e5
Kyle Fong <krfong@ucsc.edu>
2021-04-02 23:11:44 -0700

    concurrency control init. WIP: es6 promises

---
+++ frontend/javascript/concurrency-control/async-implementations/tests/promise.test.js
@@ -0,0 +1,17 @@
+import { spromise } from "../promise";
+
+describe("ES6 Promises From Scratch", () => {
+  test("Accepts an indefinite number of args", () => {});
+
+  test("Returns a pending state", () => {});
+
+  test("Result handlers are invoked: onFulfilled() or onRejected() when promise resolves ", () => {});
+
+  test("A promise resolves with the expected value", () => {});
+
+  test("We can chain promises together", () => {});
+
+  test("An error does not execute succeeding calls, and the reason for the error is returned", () => {});
+
+  test(".all[] API executes promises concurrently, and waits for all responses to return before completing execution", () => {});
+});
